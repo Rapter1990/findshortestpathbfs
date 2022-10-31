cc9dddd370057065b57a973527f33c2e2887ca8c
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-10-31 22:11:54 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/route/findshortestpathbfs/FindshortestpathbfsApplication.java
@@ -0,0 +1,13 @@
+package com.route.findshortestpathbfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindshortestpathbfsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindshortestpathbfsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/route/findshortestpathbfs/FindshortestpathbfsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.route.findshortestpathbfs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindshortestpathbfsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
